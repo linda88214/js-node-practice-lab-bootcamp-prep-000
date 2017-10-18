6dd8414db75a3c8451a01605623954618780a7c5
Yerim Hu <yerimhu88@gmail.com>
2017-10-18 02:38:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 
 var name = "Yerim"
 var height = 68
+var heelheight = "69"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height + 1)
+
+console.log("Heel Height")
+console.log(parseInt(heelheight) - 1)
